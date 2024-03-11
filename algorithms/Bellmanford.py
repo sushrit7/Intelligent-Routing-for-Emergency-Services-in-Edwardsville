3e5658608fdb7744bd883a40ce25b5cca3d74f34
“Sushrit <sushrit.k@gmail.com>
2024-03-10 23:19:51 -0500

    small changes

--- algorithms/Bellmanford.py
+++ algorithms/Bellmanford.py
@@ -1,18 +1,19 @@
 import pandas as pd
 
 def bellman_ford(graph, start):
-    # Initialize distances to all nodes as infinity
+
+    # Initializing distances to all nodes as infinity
     distances = {node: float('infinity') for node in graph}
-    distances[start] = 0  # Distance from start node to itself is 0
+    distances[start] = 0  # As distance from start node to itself is 0
     
-    # Relax edges repeatedly
+    # Relaxing edges repeatedly
     for _ in range(len(graph) - 1):
         for node in graph:
             for neighbor, weight in graph.get(node, {}).items():
                 if distances[node] + weight < distances[neighbor]:
                     distances[neighbor] = distances[node] + weight
     
-    # Check for negative cycles
+    # Checking for negative cycles
     for node in graph:
         for neighbor, weight in graph.get(node, {}).items():
             if distances[node] + weight < distances[neighbor]:
