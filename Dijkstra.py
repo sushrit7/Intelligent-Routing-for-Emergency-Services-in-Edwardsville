1f1e705cb8adc40897710f91f73cf54c4b5b4769
Nsaini772 <145401154+Nsaini772@users.noreply.github.com>
2024-04-24 21:15:32 -0500

    Update Dijkstra.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,25 +1,33 @@
+# importing packages
 import heapq
 import pandas as pd
 
+# main function
 def dijkstra(graph, start, end):
+    # initializing the dictionary with every element as infinity
     distances = {node: float('infinity') for node in graph}
     distances[start] = 0
+    # creating an empty tuple to keep track of previously visited node
     previous_nodes = {}
     priority_queue = [(0, start)]
-    
+
+    # mina loop responsible for inserting and creating distances 
     while priority_queue:
         current_distance, current_node = heapq.heappop(priority_queue)
-        
+
+        # comparing the distance with the distance from the current node 
         if current_distance > distances[current_node]:
             continue
-        
+
+        # iterating through every current node and updating the distance 
         for neighbor, weight in graph.get(current_node, {}).items():
             distance = current_distance + weight
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
                 previous_nodes[neighbor] = current_node
                 heapq.heappush(priority_queue, (distance, neighbor))
-    
+
+    # initializing the empty list to get the path stored between nodes
     shortest_path = []
     node = end
     while node is not None:
@@ -27,6 +35,7 @@ def dijkstra(graph, start, end):
         node = previous_nodes.get(node)
     shortest_path.reverse()
 
+    # returning the shortest path from teh list and distance between the node
     return distances[end], shortest_path  # Return the shortest distance and path to the end node
 
 
